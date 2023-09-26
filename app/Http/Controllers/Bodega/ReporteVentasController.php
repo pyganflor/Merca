d45c8d1d07f579ae7f2bf7aae4a347e41d716df2
RafaelPrats <rafael.pratsrecasen@gmail.com>
2023-09-26 18:19:50 -0500

    rafa

--- app/Http/Controllers/Bodega/ReporteVentasController.php
+++ app/Http/Controllers/Bodega/ReporteVentasController.php
@@ -154,25 +154,25 @@ class ReporteVentasController extends Controller
         $col = 0;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Finca');
         $col++;
-        setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Costos Armados');
-        $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Venta Armados');
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Costos Armados');
         $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Margen Armados');
         $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, '% Utilidad Armados');
         $col++;
-        setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Costos Pendientes');
-        $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Venta Pendientes');
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Costos Pendientes');
         $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Margen Pendientes');
         $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, '% Utilidad Pendientes');
         $col++;
-        setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Costos TOTAL');
-        $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Venta TOTAL');
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Costos TOTAL');
         $col++;
         setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'Margen TOTAL');
         $col++;
@@ -185,6 +185,11 @@ class ReporteVentasController extends Controller
         setBgToCeldaExcel($sheet, 'A' . $row . ':' . $columnas[$col] . $row, '00B388');
         setColorTextToCeldaExcel($sheet, 'A' . $row . ':' . $columnas[$col] . $row, 'FFFFFF');
 
+        $total_costos_armados = 0;
+        $total_venta_armados = 0;
+        $total_costos_pendientes = 0;
+        $total_venta_pendientes = 0;
+        $total_personas = 0;
         foreach ($listado as $item) {
             $margen_armados = $item['venta_armados'] - $item['costos_armados'];
             $utilidad_armados = porcentaje($margen_armados, $item['costos_armados'], 1);
@@ -196,29 +201,35 @@ class ReporteVentasController extends Controller
             $utilidad_total = porcentaje($margen_total, $total_costos, 1);
             $personas = count($item['personas']);
 
+            $total_costos_armados += $item['costos_armados'];
+            $total_venta_armados += $item['venta_armados'];
+            $total_costos_pendientes += $item['costos_pendientes'];
+            $total_venta_pendientes += $item['venta_pendientes'];
+            $total_personas += $personas;
+
             $row++;
             $col = 0;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, $item['finca']->nombre);
             $col++;
-            setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($item['costos_armados'], 2));
-            $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($item['venta_armados'], 2));
+            $col++;
+            setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($item['costos_armados'], 2));
             $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($margen_armados, 2));
             $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($utilidad_armados, 2) . '%');
             $col++;
-            setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($item['costos_pendientes'], 2));
-            $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($item['venta_pendientes'], 2));
+            $col++;
+            setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($item['costos_pendientes'], 2));
             $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($margen_pendientes, 2));
             $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($utilidad_pendientes, 2) . '%');
             $col++;
-            setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_costos, 2));
-            $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_venta, 2));
+            $col++;
+            setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_costos, 2));
             $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($margen_total, 2));
             $col++;
@@ -228,6 +239,50 @@ class ReporteVentasController extends Controller
             $col++;
             setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_venta / $personas, 2));
         }
+
+        $margen_armados = $total_venta_armados - $total_costos_armados;
+        $utilidad_armados = porcentaje($margen_armados, $total_costos_armados, 1);
+        $margen_pendientes = $total_venta_pendientes - $total_costos_pendientes;
+        $utilidad_pendientes = porcentaje($margen_pendientes, $total_costos_pendientes, 1);
+        $total_costos = $total_costos_armados + $total_costos_pendientes;
+        $total_venta = $total_venta_armados + $total_venta_pendientes;
+        $margen_total = $total_venta - $total_costos;
+        $utilidad_total = porcentaje($margen_total, $total_costos, 1);
+
+        $row++;
+        $col = 0;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, 'TOTALES');
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_venta_armados, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_costos_armados, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($margen_armados, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($utilidad_armados, 2) . '%');
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_venta_pendientes, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_costos_pendientes, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($margen_pendientes, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($utilidad_pendientes, 2) . '%');
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_venta, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_costos, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($margen_total, 2));
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($utilidad_total, 2) . '%');
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, $total_personas);
+        $col++;
+        setValueToCeldaExcel($sheet, $columnas[$col] . $row, round($total_venta / $total_personas, 2));
+
+        setBgToCeldaExcel($sheet, 'A' . $row . ':' . $columnas[$col] . $row, '00B388');
+        setColorTextToCeldaExcel($sheet, 'A' . $row . ':' . $columnas[$col] . $row, 'FFFFFF');
 
         setTextCenterToCeldaExcel($sheet, 'A1:' . $columnas[$col] . $row);
         setBorderToCeldaExcel($sheet, 'A1:' . $columnas[$col] . $row);
--- resources/views/adminlte/gestion/bodega/reporte_ventas/partials/listado.blade.php
+++ resources/views/adminlte/gestion/bodega/reporte_ventas/partials/listado.blade.php
@@ -6,11 +6,11 @@
                     Finca
                 </div>
             </th>
-            <th class="bg-yura_dark padding_lateral_5" style="width: 80px">
-                Costos Armados
-            </th>
             <th class="bg-yura_dark padding_lateral_5" style="width: 60px">
                 Venta Armados
+            </th>
+            <th class="bg-yura_dark padding_lateral_5" style="width: 80px">
+                Costos Armados
             </th>
             <th class="bg-yura_dark padding_lateral_5" style="width: 50px">
                 Margen Armados
@@ -18,11 +18,11 @@
             <th class="bg-yura_dark padding_lateral_5" style="width: 50px">
                 Utilidad Armados
             </th>
-            <th class="th_yura_green padding_lateral_5" style="width: 40px">
-                Costos Pendientes
-            </th>
             <th class="th_yura_green padding_lateral_5" style="width: 50px">
                 Venta Pendientes
+            </th>
+            <th class="th_yura_green padding_lateral_5" style="width: 40px">
+                Costos Pendientes
             </th>
             <th class="th_yura_green padding_lateral_5" style="width: 50px">
                 Margen Pendientes
@@ -30,11 +30,11 @@
             <th class="th_yura_green padding_lateral_5" style="width: 50px">
                 Utilidad Pendientes
             </th>
-            <th class="bg-yura_dark padding_lateral_5" style="width: 40px">
-                Total Costos
-            </th>
             <th class="bg-yura_dark padding_lateral_5" style="width: 50px">
                 Total Venta
+            </th>
+            <th class="bg-yura_dark padding_lateral_5" style="width: 40px">
+                Total Costos
             </th>
             <th class="bg-yura_dark padding_lateral_5" style="width: 50px">
                 Total Margen
@@ -51,6 +51,13 @@
         </tr>
     </thead>
     <tbody>
+        @php
+            $total_costos_armados = 0;
+            $total_venta_armados = 0;
+            $total_costos_pendientes = 0;
+            $total_venta_pendientes = 0;
+            $total_personas = 0;
+        @endphp
         @foreach ($listado as $item)
             @php
                 $margen_armados = $item['venta_armados'] - $item['costos_armados'];
@@ -62,16 +69,22 @@
                 $margen_total = $total_venta - $total_costos;
                 $utilidad_total = porcentaje($margen_total, $total_costos, 1);
                 $personas = count($item['personas']);
+                
+                $total_costos_armados += $item['costos_armados'];
+                $total_venta_armados += $item['venta_armados'];
+                $total_costos_pendientes += $item['costos_pendientes'];
+                $total_venta_pendientes += $item['venta_pendientes'];
+                $total_personas += $personas;
             @endphp
             <tr onmouseover="$(this).addClass('bg-yura_dark')" onmouseleave="$(this).removeClass('bg-yura_dark')">
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
                     {{ $item['finca']->nombre }}
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                    ${{ number_format($item['costos_armados'], 2) }}
+                    ${{ number_format($item['venta_armados'], 2) }}
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                    ${{ number_format($item['venta_armados'], 2) }}
+                    ${{ number_format($item['costos_armados'], 2) }}
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
                     ${{ number_format($margen_armados, 2) }}
@@ -80,10 +93,10 @@
                     {{ number_format($utilidad_armados, 2) }}%
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                    ${{ number_format($item['costos_pendientes'], 2) }}
+                    ${{ number_format($item['venta_pendientes'], 2) }}
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                    ${{ number_format($item['venta_pendientes'], 2) }}
+                    ${{ number_format($item['costos_pendientes'], 2) }}
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
                     ${{ number_format($margen_pendientes, 2) }}
@@ -92,10 +105,10 @@
                     {{ number_format($utilidad_pendientes, 2) }}%
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                    ${{ number_format($total_costos, 2) }}
+                    ${{ number_format($total_venta, 2) }}
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
-                    ${{ number_format($total_venta, 2) }}
+                    ${{ number_format($total_costos, 2) }}
                 </th>
                 <th class="padding_lateral_5" style="border-color: #9d9d9d">
                     ${{ number_format($margen_total, 2) }}
@@ -112,4 +125,61 @@
             </tr>
         @endforeach
     </tbody>
+    <tr>
+        @php
+            $margen_armados = $total_venta_armados - $total_costos_armados;
+            $utilidad_armados = porcentaje($margen_armados, $total_costos_armados, 1);
+            $margen_pendientes = $total_venta_pendientes - $total_costos_pendientes;
+            $utilidad_pendientes = porcentaje($margen_pendientes, $total_costos_pendientes, 1);
+            $total_costos = $total_costos_armados + $total_costos_pendientes;
+            $total_venta = $total_venta_armados + $total_venta_pendientes;
+            $margen_total = $total_venta - $total_costos;
+            $utilidad_total = porcentaje($margen_total, $total_costos, 1);
+        @endphp
+        <th class="padding_lateral_5 th_yura_green">
+            TOTALES
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($total_venta_armados, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($total_costos_armados, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($margen_armados, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            {{ number_format($utilidad_armados, 2) }}%
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($total_venta_pendientes, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($total_costos_pendientes, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($margen_pendientes, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            {{ number_format($utilidad_pendientes, 2) }}%
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($total_venta, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($total_costos, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($margen_total, 2) }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            {{ number_format($utilidad_total, 2) }}%
+        </th>
+        <th class="text-center th_yura_green">
+            {{ $total_personas }}
+        </th>
+        <th class="padding_lateral_5 th_yura_green">
+            ${{ number_format($total_venta / $total_personas, 2) }}
+        </th>
+    </tr>
 </table>
